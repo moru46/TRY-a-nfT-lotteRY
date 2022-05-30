79e578ae98a7f15502719c3f3996f48d9430ed5e
Davide <46970158+moru46@users.noreply.github.com>
2022-05-30 17:39:00 +0200

    Update lottery_test.js

--- lottery_test.js
+++ lottery_test.js
@@ -83,7 +83,7 @@ describe("Lottery Tests", function(){
             gas: 15000000000,
             gasPrice: '30000000000'
         })
-        console.log(try_contract.options.address)
+        console.log('Contract address ' + try_contract.options.address)
 
         let owner = await try_contract.methods.lotteryOperator().call()
         let owner_balance = await web3.eth.getBalance(owner)
@@ -103,6 +103,54 @@ describe("Lottery Tests", function(){
 
         let duration = await try_contract.methods.duration().call()
         console.log("This round will end at block: " + duration);
+
+    )};
+
+    it("User 1 buy a ticket", async function () {
+        const artifactsPath = 'contracts/artifacts/Lottery.json' // Change this for different path
+        const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath))
+        const accounts = await web3.eth.getAccounts()
+        let contract = new web3.eth.Contract(metadata.abi)
+        let failReason = null;
+        contract = contract.deploy({
+            data: metadata.data.bytecode.object,
+            arguments: [1]
+        })
+
+        try_contract = await contract.send({
+            from: accounts[0],
+            gas: 15000000000,
+            gasPrice: '30000000000'
+        })
+        console.log('Contract address ' + try_contract.options.address)
+
+        let owner = await try_contract.methods.lotteryOperator().call()
+        let owner_balance = await web3.eth.getBalance(owner)
+        console.log("Owner ----> " + owner + " With balance: " + owner_balance + " WEI")
+        
+        let isActive =  await try_contract.methods.isActive().call()
+        let prizeGiven = await try_contract.methods.prizeGiven().call()
+
+        assert.equal(isActive, false, "The round should be active")
+        assert.equal(prizeGiven, true, "The round should be active")
+
+        await try_contract.methods.startNewRound().send({
+            from: accounts[0],
+            gas: 15000000000,
+            gasPrice: '30000000000'
+        })
+
+        let duration = await try_contract.methods.duration().call()
+        console.log("This round will end at block: " + duration);
+
+        await try_contract.methods.buy([1,2,3,4,5,6]).send({
+            from: accounts[1],
+            gas: 15000000000,
+            gasPrice: '30000000000'
+            //value: 1 gwei
+        })
+
+        console.log
 
     )};
 
