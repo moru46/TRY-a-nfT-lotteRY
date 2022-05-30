29195033cd75da87c8def811436986331e046bdc
Davide <46970158+moru46@users.noreply.github.com>
2022-05-30 17:17:13 +0200

    Update lottery_test.js

--- lottery_test.js
+++ lottery_test.js
@@ -16,7 +16,7 @@ describe("Lottery Tests", function(){
 
     });
 
-    it("should be a test of the lottery", async function () {
+   /* it("Get owner balance", async function () {
         const artifactsPath = 'contracts/artifacts/Lottery.json' // Change this for different path
         const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath))
         const accounts = await web3.eth.getAccounts()
@@ -34,31 +34,80 @@ describe("Lottery Tests", function(){
         })
         console.log(try_contract.options.address)
 
-        let owner = await try_contract.methods.owner().call()
+        let owner = await try_contract.methods.lotteryOperator().call()
         let owner_balance = await web3.eth.getBalance(owner)
         console.log("Owner ----> " + owner + " With balance: " + owner_balance + " WEI")
 
 
-    });
+    });*/
 
     it("Check deployer of the contract", async function () {
-        const metadata = JSON.parse(await remix.call('fileManager', 'getFile', 'contracts/artifacts/Lottery.json'))
+        const artifactsPath = 'contracts/artifacts/Lottery.json' // Change this for different path
+        const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath))
         const accounts = await web3.eth.getAccounts()
+        let contract = new web3.eth.Contract(metadata.abi)
+        let failReason = null;
+        contract = contract.deploy({
+            data: metadata.data.bytecode.object,
+            arguments: [1]
+        })
 
-        const signer = (new ethers.providers.Web3Provider(web3Provider)).getSigner();
-        let Try = new ethers.ContractFactory(metadata.abi, metadata.data.bytecode.object, signer);
-        let try_contract = await Try.deploy(1);
-        //console.log('Storage contract Address: ' + try_contract.address);
-        await try_contract.deployed();
+        try_contract = await contract.send({
+            from: accounts[0],
+            gas: 15000000000,
+            gasPrice: '30000000000'
+        })
+        console.log(try_contract.options.address)
 
-        var manager_address = await try_contract.owner();
+        var manager_address = await try_contract.methods.lotteryOperator().call()
         var deployer_address = accounts[0];
 
         assert.equal(manager_address,deployer_address, "manager is not deployer");
 
-
-
     });
+
+   
+    it("Test initial values and start round", async function () {
+        const artifactsPath = 'contracts/artifacts/Lottery.json' // Change this for different path
+        const metadata = JSON.parse(await remix.call('fileManager', 'getFile', artifactsPath))
+        const accounts = await web3.eth.getAccounts()
+        let contract = new web3.eth.Contract(metadata.abi)
+        let failReason = null;
+        contract = contract.deploy({
+            data: metadata.data.bytecode.object,
+            arguments: [1]
+        })
+
+        try_contract = await contract.send({
+            from: accounts[0],
+            gas: 15000000000,
+            gasPrice: '30000000000'
+        })
+        console.log(try_contract.options.address)
+
+        let owner = await try_contract.methods.lotteryOperator().call()
+        let owner_balance = await web3.eth.getBalance(owner)
+        console.log("Owner ----> " + owner + " With balance: " + owner_balance + " WEI")
+        
+        let isActive =  await try_contract.methods.isActive().call()
+        let prizeGiven = await try_contract.methods.prizeGiven().call()
+
+        assert.equal(isActive, false, "The round should be active")
+        assert.equal(prizeGiven, true, "The round should be active")
+
+        await try_contract.methods.startNewRound().send({
+            from: accounts[0],
+            gas: 15000000000,
+            gasPrice: '30000000000'
+        })
+
+        let duration = await try_contract.methods.duration().call()
+        console.log("This round will end at block: " + duration);
+
+    )};
+
+
+    
 
     
 });
