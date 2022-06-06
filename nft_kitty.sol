325b2c5ab4be17435294073d9ed6d023bde70486
Davide <46970158+moru46@users.noreply.github.com>
2022-06-06 15:30:17 +0200

    Update nft_kitty.sol

--- nft_kitty.sol
+++ nft_kitty.sol
@@ -16,10 +16,10 @@ contract kittyNft is ERC721URIStorage {
 
     constructor() ERC721("kittyNft", "ITM") {}
 
+    //function to mint a new nft of class X
     function mint(uint class) public returns (uint256)
     {
         uint256 newItemId = _tokenIds.current();
-        //_setTokenURI(newItemId, tokenURI);
         _tokenIds.increment();
         rank[class] = newItemId;
         description[class] = string(abi.encodePacked("NFT class: ", Strings.toString(class)));
@@ -30,6 +30,7 @@ contract kittyNft is ERC721URIStorage {
         return rank[class];
     }
 
+    //function to give the prize to the player who won it
     function awardItem(address player, uint256 tokendId) public {
         _mint(player, tokendId);
     }
